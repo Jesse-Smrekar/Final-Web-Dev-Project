7ab03973967e8a12a1e037391712e8ea29418cf9
IslandIdealist <IslandIdealist@gmail.com>
2019-12-11 00:56:36 -0600

    Added tags, implemented get request to RESTFul sevrer, halfway implemented table..

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,15 @@ var neighborhoods = [
 ];
 
 
+
+
+
+
+
+
+
+
+
 //NODE COMMAND: static-server -p 8001
 
 function Init(crime_api_url) {
@@ -31,6 +40,7 @@ function Init(crime_api_url) {
     app = new Vue({
         el: "#app",
         data: {
+						rows: {},
             location_search: "",
 			map: mymap,
 			search_type: "address",
@@ -43,42 +53,27 @@ function Init(crime_api_url) {
         computed: {
             search_placeholder: function() {
 				//var mapCenter = map.getCenter().toString();
-	
+
                 if (this.search_type[0] === "a")
                     return "Search for an Address: 1234 Street";
-                return "Search for uh coordinate"; //mapCenter.substring( mapCenter.indexOf('(') + 1, mapCenter.length - 1);	
+                return "Search for uh coordinate"; //mapCenter.substring( mapCenter.indexOf('(') + 1, mapCenter.length - 1);
 
             }
         }
     });
 
-		mymap = L.map('map').setView([44.954179, -93.091797], 2);
+		createMap();
+		getCrimeData();
 
-		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXNsYW5kaWRlYWxpc3QiLCJhIjoiY2szdzVhMHhtMGVpMDNrcGVtM3I3dTR1NCJ9.TB0u05RvP7KUzTvyumY4XA', {
-			maxZoom: 18,
-			minZoom: 12,
-			scrollWheelZoom: true,
-			touchZoom: 'center',
-			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-			id: 'mapbox/streets-v11'
-		}).addTo(mymap);
-
-		var northWest = L.latLng(45.002773, -93.218671), southEast = L.latLng(44.883764, -92.991386);
-		var bounds = L.latLngBounds(northWest, southEast);
-
-		mymap.setMaxBounds(bounds);
-		mymap.on('drag', function() {
-		    mymap.panInsideBounds(bounds, { animate: false });
-		});
-		
-		for(var i=0; i< neighborhoods.length; i++){
-			L.marker([neighborhoods[i].lat, neighborhoods[i].lon]).bindTooltip(neighborhoods[i].hood, { permanent: false, direction: 'top'}).addTo(mymap);			
-		}
-		
-		
 }
+
+
+
+
+
+
+
+
 
 function Prompt() {
 		$(".dialog-form").dialog({
@@ -104,7 +99,7 @@ function Prompt() {
 function LocationSearch(event)
 {
 	var input = app.location_search;
-	
+
 	if( app.search_type == "address"){
 		let result = 'street=' + app.location_search + '&city=Saint+Paul&format=json';
 		getLocation(result);
@@ -138,13 +133,95 @@ function displayCoordinates(data){
 }
 
 
+
+function getCrimeData(){
+
+	let request = {
+			url: 'http://cisc-dean.stthomas.edu:8002/incidents?format=json&limit=10',
+			headers: { "Accept": "application/json"},
+			//url: API_URL + '/incidents?format=json',//"https://cisc-dean.stthomas.edu:" + port,
+			dataType: "json",
+			type: 'GET',
+			crossDomain: true,
+      beforeSend: function(xhr){
+          xhr.withCredentials = true;
+    	},
+			success: populateRows,
+			error: function(){ alert('Could Not Get St.Paul Crime Data');}
+	};
+
+	$.ajax(request);
+}
+
+function populateRows(data){
+
+	alert(JSON.stringify(data));
+	app.rows = data;
+
+}
+
+function createMap(){
+
+
+			mymap = L.map('map').setView([44.954179, -93.091797], 2);
+
+			L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXNsYW5kaWRlYWxpc3QiLCJhIjoiY2szdzVhMHhtMGVpMDNrcGVtM3I3dTR1NCJ9.TB0u05RvP7KUzTvyumY4XA', {
+				maxZoom: 18,
+				minZoom: 12,
+				scrollWheelZoom: true,
+				touchZoom: 'center',
+				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+					'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+					'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+				id: 'mapbox/streets-v11'
+			}).addTo(mymap);
+
+			var northWest = L.latLng(45.002773, -93.218671), southEast = L.latLng(44.883764, -92.991386);
+			var bounds = L.latLngBounds(northWest, southEast);
+
+			mymap.setMaxBounds(bounds);
+			mymap.on('drag', function() {
+			    mymap.panInsideBounds(bounds, { animate: false });
+			});
+
+			for(var i=0; i< neighborhoods.length; i++){
+				L.marker([neighborhoods[i].lat, neighborhoods[i].lon]).bindTooltip(neighborhoods[i].hood, { permanent: false, direction: 'top'}).addTo(mymap);
+			}
+
+			var polygon = L.polygon(
+	    [[[90, -180],
+	      [90, 180],
+	      [-90, 180],
+	      [-90, -180]], //outer ring
+	     [[44.991639, -93.207340],
+	      [44.953294, -93.207336],
+				[44.900303, -93.193946],
+				 [44.894234, -93.169502],
+				 [44.920727, -93.117729],
+				 [44.919764, -93.096051],
+				 [44.923644, -93.096029],
+				 [44.923573, -93.091014],
+				 [44.919685, -93.090924],
+				 [44.918921, -93.050867],
+				 [44.891048, -93.022994],
+				 [44.891052, -93.004513],
+				 [44.992155, -93.005119],
+	      [44.991696, -93.166690]]] // cutout
+	    ).setStyle({fillColor:"#334652", fillOpacity: 0.7}).addTo(mymap);
+
+	//44.991696, -93.166690
+
+
+
+}
+
 /* Neighborhoods
 
 
 
 
-Highland Park: 44.912641, -93.177235
-Macalenser-Groveland: 44.934347, -93.173582
+Highland Park: [44.912641, -93.177235]
+Macalenser-Groveland: [44.934347, -93.173582]
 West Seventh: 44.927815, -93.126994
 Riverview: 44.933023, -93.090391
 Daytons Bluff: 44.955243, -93.060887
@@ -161,5 +238,3 @@ Battle Creek: 44.944011, -93.025156
 
 
 */
-
-
